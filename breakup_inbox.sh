1defd9dfe0e02591946f7dff70d1dc70c64076f9
Rob Seastrom <rs@cluetrust.com>
2023-06-15 16:52:48 +0000

    remove explicit paths in breakup_inbox.sh, add admonition to put this directory on your /home/rs/maildir-clitools:/usr/local/sbin:/usr/local/bin:/opt/local/sbin:/opt/local/bin:/usr/sbin:/usr/bin:/sbin before proceeding

--- breakup_inbox.sh
+++ breakup_inbox.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
-
+#
+# first set your path to include the git directory...
+#
+# [root@staging /home/rs/maildir-clitools]# PATH=`pwd`:$PATH
+#
 # usage: breakup_inbox.sh rs 2016 2022
 #        breakup_inbox.sh user first_year final_year (to archive)
 
@@ -11,7 +15,7 @@ do
   date
   echo "archiving $year"
   doveadm mailbox create -u $1 Archives$year
-  time ls | /home/$1/maildirfind.sh $year | /home/$1/bulkrename /home/$1/Maildir/.Archives$year/cur
+  time ls | maildirfind.sh $year | bulkrename /home/$1/Maildir/.Archives$year/cur
   date
   echo "indexing $year (please be patient)"
   time doveadm index -u $1 Archives$year
